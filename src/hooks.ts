a2028b27b31fc0e8d8992b47c4b94808cbb85a76
El Mehdi Rami <lbiggboss19@gmail.com>
2020-09-17 22:07:32 +0100

    About component complete

---
+++ src/hooks.ts
@@ -0,0 +1,26 @@
+// interface ResponseError {
+//   success: false;
+//   message: string;
+// }
+
+type endpoint = "blog" | "project" | "projects" | "skills";
+type variables = { [key: string]: string | number } | undefined;
+
+export const useFetch = async <T>(
+  endpoint: endpoint,
+  variables: variables = undefined
+): Promise<T | null> => {
+  let base: string = `/api/${endpoint}`;
+  if (endpoint === "project") {
+    const { id } = variables as any;
+    base += `/${id}`;
+  }
+
+  try {
+    const response = await fetch(base);
+    const body = await response.json();
+    return body;
+  } catch (e) {
+    return null;
+  }
+};
