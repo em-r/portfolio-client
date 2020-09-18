4c592c10f1c7a662389573a79e11b8fadaadf5c7
El Mehdi Rami <lbiggboss19@gmail.com>
2020-09-18 21:49:08 +0100

    Removed dummy data

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -3,21 +3,6 @@ import { Main } from "../../styles/Main";
 import ProjectSummary from "./Summary";
 import { useFetch } from "../../hooks";
 
-// const projects = [
-//   {
-//     title: "HelloFlask",
-//     description:
-//       "HelloFlask is a CLI tool written in Python, that automates creating a Flask project and setting up its virtual environment",
-//     stack: ["Python"],
-//     links: {
-//       PyPi: "https://pypi.org/project/HelloFlask/",
-//       GitHub: "https://github.com/ElMehdi19/HelloFlask/tree/master/helloflask",
-//     },
-//     thumbnail:
-//       "https://www.pngfind.com/pngs/m/104-1044449_python-logo-clipart-drawing-flask-python-hd-png.png",
-//   },
-// ];
-
 type Project = {
   title: string;
   description: string;
