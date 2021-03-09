78e5e882d581440e510f04e937991d5adc1fa2e5
El Mehdi Rami <lbiggboss19@gmail.com>
2021-03-09 02:47:28 +0100

    added blog read more button redirection

--- src/components/main/HomeBlog.tsx
+++ src/components/main/HomeBlog.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Redirect } from "react-router-dom";
 import { globalContext } from "../../store/globalContext";
 import { Main } from "../../styles/Main";
 import Blog from "../blog/Blog";
@@ -8,13 +8,18 @@ const Home: React.FC = () => {
   const {
     globalState: { blogsLoaded },
   } = useContext(globalContext);
+
+  const [redirect, setRedirect] = useState<boolean>(false);
+
+  if (redirect) return <Redirect to="/blog" />;
+
   return (
     <Main width="800px">
       <Blog />
       {blogsLoaded && (
-        <Link to="/blog" style={{ textAlign: "center" }}>
-          Read more
-        </Link>
+        <p style={{ textAlign: "center" }}>
+          <button onClick={() => setRedirect(!redirect)}>Read more</button>
+        </p>
       )}
     </Main>
   );
