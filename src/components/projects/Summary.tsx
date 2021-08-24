a6c68e7706d91be5d4a8d20f328346ea9b1570f8
El Mehdi Rami <mehdi@httphub.io>
2021-08-24 14:28:22 +0100

    styling project links

--- src/components/projects/Summary.tsx
+++ src/components/projects/Summary.tsx
@@ -1,12 +1,56 @@
 import React from "react";
+import {
+  DiGithub,
+  DiPython,
+  DiMarkdown,
+  DiDocker,
+  DiWebplatform,
+} from "react-icons/di";
 
-interface Props {
+type Props = {
   name: string;
   description: string;
   stack: string[];
   links: { [key: string]: string };
   thumbnail: string;
-}
+};
+
+const LinkIcon: React.FC<{ id: string; dim: number }> = ({ id, dim }) => {
+  const title = id.toLowerCase().split(" ");
+  switch (title[0]) {
+    case "github":
+      return <DiGithub size={dim} />;
+    case "pypi":
+      return <DiPython size={dim} />;
+    case "docker":
+      return <DiDocker size={dim} />;
+    case "documentation":
+      return <DiMarkdown size={dim} />;
+    case "live":
+      return <DiWebplatform size={dim} />;
+    default:
+      return null;
+  }
+};
+
+const ProjectLink: React.FC<{ id: string; url: string }> = ({ id, url }) => {
+  const style: React.CSSProperties = {
+    display: "flex",
+    alignItems: "center",
+    gap: 5,
+  };
+  return (
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{ ...style }}
+    >
+      <LinkIcon id={id} dim={50} />
+      <span>{id}</span>
+    </a>
+  );
+};
 
 const ProjectSummary: React.FC<Props> = ({
   name,
@@ -25,15 +69,7 @@ const ProjectSummary: React.FC<Props> = ({
         <div>
           <h4>Links</h4>
           {linksTo.map((link) => (
-            <p key={link}>
-              <a
-                href={`${links[link]}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {link}
-              </a>
-            </p>
+            <ProjectLink id={link} url={links[link]} key={link} />
           ))}
         </div>
       </div>
